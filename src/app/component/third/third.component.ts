0e7d8336dcba144ada3fc8aff2e6d0b3ce8d0f3f
Pentakilj97 <127298119+Pentakilj97@users.noreply.github.com>
2023-06-05 10:10:41 +0200

    third commit

--- src/app/component/third/third.component.ts
+++ src/app/component/third/third.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DataService } from 'src/app/services/data/data-service.service';
 
 @Component({
   selector: 'app-third',
   templateUrl: './third.component.html',
   styleUrls: ['./third.component.scss']
 })
-export class ThirdComponent {
+export class ThirdComponent implements OnInit {
 
+  isEven: boolean = true;
+  constructor(public data: DataService) {
+
+  };
+  ngOnInit(): void {
+    this.data.counter.subscribe({
+      next: (value: number) => {
+        const square = value * value;
+        if (square % 2 === 0) {
+          this.isEven = true;
+        } else {
+          this.isEven = false;
+        }
+      }
+    })
+  }
 }
