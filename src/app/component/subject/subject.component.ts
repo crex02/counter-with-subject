53ea49b59d44192115f6ead6b03e8656fdff2de7
Pentakilj97 <127298119+Pentakilj97@users.noreply.github.com>
2023-06-05 11:43:18 +0200

    final

--- src/app/component/subject/subject.component.ts
+++ src/app/component/subject/subject.component.ts
@@ -11,21 +11,27 @@ export class SubjectComponent implements OnInit {
   constructor(private rxjsServ: RxjsService) { }
 
   ngOnInit(): void {
-    
-    this.rxjsServ.subjectVariable.subscribe({
-      next: (value: string) => console.log('subject', value),
-      error: err => console.log(err),
-    }),
 
-    this.rxjsServ.behaviorVariable.subscribe({
-      next: (value: string) => console.log('behaviorSubject', value),
-      error: err => console.log(err),
-    }),
+    // this.rxjsServ.subjectVariable.subscribe({
+    //   next: (value: string) => console.log('subject', value),
+    //   error: err => console.log(err),
+    // }),
 
-    this.rxjsServ.replayVariable.subscribe({
-      next: (value: string) => console.log('replaySubject', value),
-      error: err => console.log(err),
-    })
+    // this.rxjsServ.behaviorVariable.subscribe({
+    //   next: (value: string) => console.log('behaviorSubject', value),
+    //   error: err => console.log(err),
+    // }),
+
+    // this.rxjsServ.replayVariable.subscribe({
+    //   next: (value: string) => console.log('replaySubject', value),
+    //   error: err => console.log(err),
+    // })
+
+    setTimeout(() => {
+      this.rxjsServ.subjectVariable.subscribe({
+       next: (value: string) => console.log('subject', value),
+       error: err => console.log(err)});
+    }, 6000)
 
   }
 }
